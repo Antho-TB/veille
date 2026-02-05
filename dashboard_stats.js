6b42807160d4d8cbdea1b548870dc54519ecc1b5
Anthony <201716998+Antho-TB@users.noreply.github.com>
2026-02-05 11:10:55 +0100

    Fix: Categorized KPIs and reliable JS data injection for local dashboard.

---
+++ dashboard_stats.js
@@ -0,0 +1,38 @@
+var DASHBOARD_DATA = {
+    "last_update": "05/02/2026 11:10",
+    "kpis": {
+        "total_tracked": 994,
+        "applicable": 291,
+        "actions_required": 269,
+        "sub_mec": 8,
+        "sub_reeval": 261,
+        "sub_qualif": 0,
+        "new_alerts": 47
+    },
+    "themes": {
+        "labels": [
+            "DIVERS",
+            "ICPE\nGénéralités - Classement",
+            "AUTRES EXIGENCES\nFiscalité énergétique",
+            "EAU\nGénéralités & IOTA",
+            "RISQUES\nProduits chimiques\nREACH",
+            "RISQUES\nEquipements sous pression",
+            "AUTRES EXIGENCES\nSilence de l'administration vaut accord ou rejet",
+            "RISQUES\nAutorisation environnementale",
+            "AIR\nCadre législatif",
+            "RISQUES\nEnquête publique"
+        ],
+        "values": [
+            119,
+            38,
+            37,
+            35,
+            26,
+            25,
+            25,
+            21,
+            21,
+            21
+        ]
+    }
+};
