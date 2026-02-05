b2fd69f44a655ce739fe9a2017095a286f3f31d4
Anthony <201716998+Antho-TB@users.noreply.github.com>
2026-02-05 16:59:26 +0100

    feat: Dashboard professionalization, theme refinement (<10% Divers), and Methodology tab integration for ISO 14001 readiness

--- output/dashboard_stats.js
+++ output/dashboard_stats.js
@@ -1,42 +1,42 @@
 var DASHBOARD_DATA = {
-    "last_update": "05/02/2026 16:16",
+    "last_update": "05/02/2026 16:57",
     "kpis": {
         "total_tracked": 994,
         "applicable": 291,
-        "actions_required": 269,
-        "sub_mec": 8,
+        "actions_required": 261,
+        "sub_mec": 0,
         "sub_reeval": 261,
         "sub_qualif": 0,
         "new_alerts": 49
     },
     "themes": {
         "labels": [
-            "RISQUES",
-            "DIVERS",
-            "DECHETS",
-            "ICPE",
+            "ADMINISTRATION / GOUVERNANCE",
+            "RISQUES & SÉCURITÉ",
+            "ICPE / IOTA",
+            "RSE & SUBSTANCES",
+            "DÉCHETS / REP",
             "EAU",
+            "TRANSPORT / ADR",
             "AIR",
-            "ADMINISTRATION",
-            "DÉCHETS - REP",
-            "BRUIT\nCADRE LÉGISLATIF",
-            "DÉCHETS - REP / EMBALLAGES",
-            "DROIT - ADMINISTRATION",
-            "DÉCHETS - REP / PRÉVENTION"
+            "ÉNERGIE",
+            "SOLS / INFRASTRUCTURES",
+            "SÉCURITÉ / SANTÉ",
+            "DÉCHETS"
         ],
         "values": [
-            288,
             286,
-            113,
-            105,
-            77,
-            58,
-            15,
-            15,
-            5,
-            5,
-            4,
-            3
+            185,
+            96,
+            95,
+            91,
+            76,
+            69,
+            62,
+            18,
+            9,
+            6,
+            1
         ]
     },
     "compliance": {
